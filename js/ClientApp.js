cc9400d051a3b0950199aa057cf226195c59225b
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-02-13 20:13:36 +0900

    Add babel loader and babel configuration for React and ES2015

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,4 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(React.createElement('h1', {}, 'Hello world'), document.getElementById('app'))
+const HelloWorld = React.createClass({
+  render () {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+})
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'))
