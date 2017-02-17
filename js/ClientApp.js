a6b9aadbef3051c9109b853fea5edac203505095
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-02-17 11:59:42 +0900

    Add HomePage

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import '../css/normalize'
-import '../css/style'
+import '../css/HomePage'
 
-const HelloWorld = React.createClass({
+const HomePage = React.createClass({
   render () {
     return (
-      <h1>Hello World</h1>
+      <div className="HomePage">
+        <div className="Introduction">
+          <div className="Introduction__name">Sebastian Muszynski</div>
+          <div className="Introduction__description">Front-End Web Developer</div>
+        </div>
+        <div className="SocialIcons">
+          <a href="https://www.linkedin.com/in/muszynskisebastian" target="blank">
+            <img className="SocialIcons__linkedin" src="../img/icon-linkedin.png" alt="LinkedIn" />
+          </a>
+        </div>
+      </div>
     )
   }
 })
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'))
+ReactDOM.render(<HomePage />, document.getElementById('app'))
