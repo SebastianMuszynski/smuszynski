4972706ac3e4c7a57504c6a9123483d7f3a2c803
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-02-17 12:01:25 +0900

    Use singlequotes only

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -5,14 +5,14 @@ import '../css/HomePage'
 const HomePage = React.createClass({
   render () {
     return (
-      <div className="HomePage">
-        <div className="Introduction">
-          <div className="Introduction__name">Sebastian Muszynski</div>
-          <div className="Introduction__description">Front-End Web Developer</div>
+      <div className='HomePage'>
+        <div className='Introduction'>
+          <div className='Introduction__name'>Sebastian Muszynski</div>
+          <div className='Introduction__description'>Front-End Web Developer</div>
         </div>
-        <div className="SocialIcons">
-          <a href="https://www.linkedin.com/in/muszynskisebastian" target="blank">
-            <img className="SocialIcons__linkedin" src="../img/icon-linkedin.png" alt="LinkedIn" />
+        <div className='SocialIcons'>
+          <a href='https://www.linkedin.com/in/muszynskisebastian' target='blank'>
+            <img className='SocialIcons__linkedin' src='../img/icon-linkedin.png' alt='LinkedIn' />
           </a>
         </div>
       </div>
